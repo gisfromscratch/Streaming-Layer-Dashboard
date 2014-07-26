6ffa9062c807ca833b933c4b084f8e1919ebc8f8
J.T <gisfromscratch@live.de>
2014-07-26 20:08:10 +0200

    Added a stream message class containing the serialized features

---
+++ Platforms/DotNet/Streaming Layer Dashboard/ArcGis.Runtime.Service.Client/IStreamVisitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcGis.Runtime.Service.Client
+{
+    /// <summary>
+    /// Visitor for messages delivered by a Streaming Service.
+    /// </summary>
+    public interface IStreamVisitor
+    {
+        /// <summary>
+        /// Visits a stream message delivered by a Streaming Service.
+        /// </summary>
+        /// <param name="message">The message containing the serialized features.</param>
+        /// <returns><c>false</c> if the next stream message should not be delegated to this visitor instance.</returns>
+        bool visit(StreamMessage message);
+    }
+}
---
+++ Platforms/DotNet/Streaming Layer Dashboard/ArcGis.Runtime.Service.Client/StreamMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcGis.Runtime.Service.Client
+{
+    /// <summary>
+    /// Message from a Streaming Service.
+    /// </summary>
+    public class StreamMessage
+    {
+        private readonly byte[] _buffer;
+
+        internal StreamMessage(byte[] buffer)
+        {
+            _buffer = buffer;
+        }
+
+        /// <summary>
+        /// Raw bytes containing the serialized features.
+        /// </summary>
+        public byte[] RawBytes
+        {
+            get
+            {
+                return _buffer;
+            }
+        }
+    }
+}

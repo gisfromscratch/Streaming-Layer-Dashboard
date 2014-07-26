024a5ca9248b9e598a004e197896d5ecad3bed3f
J.T <gisfromscratch@live.de>
2014-07-26 15:34:45 +0200

    Initial commit

---
+++ Platforms/DotNet/Streaming Layer Dashboard/ArcGis.Runtime.Service.Client/StreamingLayer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Esri.ArcGISRuntime.Layers;
+
+namespace ArcGis.Runtime.Service.Client
+{
+    /// <summary>
+    /// Streaming Layer using WebSocket to connect with GeoEvent Processor.
+    /// </summary>
+    public class StreamingLayer : GraphicsLayer
+    {
+        private readonly ClientWebSocket _socket;
+
+        private readonly Uri _url;
+
+        public StreamingLayer(String url)
+        {
+            _socket = new ClientWebSocket();
+            _url = new Uri(url);
+        }
+
+        /// <summary>
+        /// Starts connecting to the Streaming Service,
+        /// </summary>
+        /// <param name="cancellationToken">The cancelation token for this request.</param>
+        public async Task ConnectAsync(CancellationToken cancellationToken)
+        {
+            await _socket.ConnectAsync(_url, cancellationToken);
+
+            const int ChunkSize = 1024;
+            var buffer = new byte[ChunkSize];
+            var ready = true;
+            while (ready)
+            {
+                var bufferSegment = new ArraySegment<byte>(buffer);
+                var messageTask = await _socket.ReceiveAsync(bufferSegment, cancellationToken);
+                switch (messageTask.MessageType)
+                {
+                    case WebSocketMessageType.Close:
+                        ready = false;
+                        break;
+                }
+
+                var messageSize = messageTask.Count;
+                while (!messageTask.EndOfMessage)
+                {
+                    if (buffer.Length <= messageSize)
+                    {
+                        // TODO: Handle Invalid Payload
+                        await _socket.CloseAsync(WebSocketCloseStatus.InvalidPayloadData, "Buffer exceeded!", CancellationToken.None);
+                        return;
+                    }
+
+                    bufferSegment = new ArraySegment<byte>(buffer, messageSize, buffer.Length - messageSize);
+                    messageTask = await _socket.ReceiveAsync(bufferSegment, cancellationToken);
+                    messageSize += messageTask.Count;
+                }
+
+                // TODO: Analyze message
+                var messageAsUtf8 = Encoding.UTF8.GetString(buffer);
+                Trace.WriteLine(messageAsUtf8);
+            }
+        }
+
+        /// <summary>
+        /// Starts closing the connection to the Streaming Service.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token for this request.</param>
+        public async void CloseAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Status Description should be changed
+            await _socket.CloseAsync(WebSocketCloseStatus.NormalClosure, @"OK", cancellationToken);
+        }
+    }
+}
---
+++ Platforms/DotNet/Streaming Layer Dashboard/Client Testing/StreamingLayerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ArcGis.Runtime.Service.Client;
+
+namespace Client_Testing
+{
+    [TestClass]
+    public class StreamingLayerTests
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+            var streamingLayer = new StreamingLayer(@"ws://geoeventsample1.esri.com:8080/exactearthservice");
+            var task = streamingLayer.ConnectAsync(CancellationToken.None);
+            task.Wait();
+        }
+    }
+}
